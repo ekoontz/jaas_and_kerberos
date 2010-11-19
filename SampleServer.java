86df6a3ae9c5e27cd81dc0c4d2c8c76d753960b0
Eugene Koontz <ekoontz@hiro-tan.org>
2010-11-19 12:14:44 -0800

    copy more stuff from Server.java

--- SampleServer.java
+++ SampleServer.java
@@ -3,15 +3,38 @@ import java.io.*;
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.util.Properties;
+import javax.security.auth.Subject;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+import javamonkey.app.gss.LoginCallbackHandler;
 
 public class SampleServer  {
     
     public static void main(String[] args) 
 	throws IOException, GSSException {
 
+      // Setup up the Kerberos properties.
+      Properties props = new Properties();
+      props.load( new FileInputStream( "server.properties"));
       System.setProperty( "sun.security.krb5.debug", "true");
+      System.setProperty( "java.security.auth.login.config", "./jaas.conf");
+      System.setProperty( "javax.security.auth.useSubjectCredsOnly", "true");
+      String password = props.getProperty( "service.password");
+      Oid krb5Oid = new Oid( "1.2.840.113554.1.2.2");
 
-
+      // Login to the KDC.
+      LoginContext loginCtx = null;
+      // "Client" references the JAAS configuration in the jaas.conf file.
+      try {
+        loginCtx = new LoginContext( "SampleServer",
+                                     new LoginCallbackHandler( password));
+        loginCtx.login();
+        Subject subject = loginCtx.getSubject();
+      }
+      catch (LoginException e) {
+        System.err.println("Login failure : " + e);
+        System.exit(-1);
+      }
 	// Obtain the command-line arguments and parse the port number
 
 	if (args.length != 1) {
@@ -38,11 +61,12 @@ public class SampleServer  {
 	    System.out.println("SampleServer::main() Got connection from client "
 			       + socket.getInetAddress());
 
-            // do stuff...
 	    GSSContext context = manager.createContext((GSSCredential)null);
+            context.requestMutualAuth(true);  // Mutual authentication
+            context.requestConf(true);  // Will use confidentiality later
+            context.requestInteg(true); // Will use integrity later
 
 	    // Do the context establishment loop.
-	    
 	    byte[] token = null;
 	    
 	    while (!context.isEstablished()) {
