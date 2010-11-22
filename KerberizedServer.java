b2a2e6454cc2d12a58d29c7826ef3b8e609955f7
Eugene Koontz <ekoontz@hiro-tan.org>
2010-11-22 10:05:44 -0800

    first version that successfully accepts security context from client.

--- KerberizedServer.java
+++ KerberizedServer.java
@@ -86,13 +86,20 @@ public class KerberizedServer {
     serverChannel.register(selector, SelectionKey.OP_ACCEPT);
     // </1. NIO>
 
+    GSSContext clientContext = null;
+
     System.out.println("start main listen loop..");
     while(true) {
+
+      if (clientContext != null) {
+        System.out.println("client context set; breaking out of loop.");
+        break;
+      }
 
       selector.select();
       Iterator selectedKeys = selector.selectedKeys().iterator();
       while (selectedKeys.hasNext()) {
-        SelectionKey key = (SelectionKey) selectedKeys.next();
+        final SelectionKey key = (SelectionKey) selectedKeys.next();
         selectedKeys.remove();
 
         if (!key.isValid()) {
@@ -117,72 +124,62 @@ public class KerberizedServer {
 
         } else if (key.isReadable()) {
           System.out.println("key is readable.");
-          SocketChannel socketChannel = (SocketChannel) key.channel();
+          final SocketChannel socketChannel = (SocketChannel) key.channel();
 
-          if (true) {
-            ByteBuffer readBuffer = ByteBuffer.allocate(8192);
-            readBuffer.clear();
-            
-            // Attempt to read off the channel
-            int numRead = 0;
-            try {
-              numRead = socketChannel.read(readBuffer);
-              if (numRead != -1) {
-                readBuffer.flip();
-                System.out.println("read: " + numRead + " bytes.");
-                byte[] bytes = new byte[8192];
-                readBuffer.get(bytes,0,numRead);
-                Hexdump.hexdump(System.out,bytes,0,numRead);
-              }
-            } catch (IOException e) {
-              // The remote forcibly closed the connection, cancel
-              // the selection key and close the channel.
-              key.cancel();
-              socketChannel.close();
-            }
-            
-            if (numRead == -1) {
-              // Remote entity shut the socket down cleanly. Do the
-              // same from our end and cancel the channel.
-              key.channel().close();
-              key.cancel();
-            }
-          }
-          else {
-            // streams do not seem to work well with NIO, perhaps because:
-            // "If the mechanism token contains a definitive start and end [,]
-            // this method may block on the InputStream if only part of the token is available."
-            // http://www.exciton.cs.rice.edu/javadocs/docs/api/org/ietf/jgss/GSSContext.html#acceptSecContext%28java.io.InputStream,%20java.io.OutputStream%29
-            
-            Socket socket = socketChannel.socket();
-            final DataInputStream inStream = new DataInputStream(socket.getInputStream());          
-            final DataOutputStream outStream = new DataOutputStream(socket.getOutputStream());          
-            
-            // 3.3 get client's security context
-            GSSContext clientContext =
-              Subject.doAs( subject, new PrivilegedAction<GSSContext>() {
-                  public GSSContext run() {
-                    try {
-                      GSSManager manager = GSSManager.getInstance();
-                      GSSContext context = manager.createContext( (GSSCredential) null);
-                      while (!context.isEstablished()) {
-                        System.out.println("KerberizedServer: context not yet established: accepting from client.");
-                        context.acceptSecContext(inStream,outStream);
+          clientContext =
+            Subject.doAs( subject, new PrivilegedAction<GSSContext>() {
+                public GSSContext run() {
+                  try {
+                    GSSManager manager = GSSManager.getInstance();
+                    GSSContext context = manager.createContext( (GSSCredential) null);
+                    while (!context.isEstablished()) {
+                      System.out.println("KerberizedServer: context not yet established: accepting from client.");
+                      
+                      ByteBuffer readBuffer = ByteBuffer.allocate(8192);
+                      readBuffer.clear();
+                      
+                      // Attempt to read off the channel
+                      int numRead = 0;
+                      try {
+                        numRead = socketChannel.read(readBuffer);
+                        if (numRead != -1) {
+                          readBuffer.flip();
+                          System.out.println("read: " + numRead + " bytes.");
+                          byte[] bytes = new byte[8192];
+                          readBuffer.get(bytes,0,numRead);
+                          Hexdump.hexdump(System.out,bytes,0,numRead);
+                          context.acceptSecContext(bytes,0,numRead);
+                          System.out.println("called acceptSecContext.");
+                        }
+                      } catch (IOException e) {
+                        // The remote forcibly closed the connection, cancel
+                        // the selection key and close the channel.
+                        key.cancel();
+                        socketChannel.close();
                       }
                       
-                      return context;
+                      if (numRead == -1) {
+                        // Remote entity shut the socket down cleanly. Do the
+                        // same from our end and cancel the channel.
+                        key.channel().close();
+                        key.cancel();
+                      }
                     }
-                    catch ( Exception e) {
-                      e.printStackTrace();
-                      return null;
-                    }
+                    System.out.println("returning context now.");
+                    return context;
                   }
-                });
-            
-            if (clientContext != null) {
-              System.out.println("KerberizedServer: Client authenticated: (principal: " + clientContext.getSrcName() + ")");
-              // ..conduct business with client since it's authenticated and optionally encrypted too..
-            }
+                  catch ( Exception e) {
+                    e.printStackTrace();
+                    return null;
+                  }
+                }
+              });
+
+
+          System.out.println("out of doAs() loop....");
+          if (clientContext != null) {
+            System.out.println("KerberizedServer: Client authenticated: (principal: " + clientContext.getSrcName() + ")");
+            // ..conduct business with client since it's authenticated and optionally encrypted too..
           }
         } else if (key.isWritable()) {
           //          System.out.println("key is writeable.");
