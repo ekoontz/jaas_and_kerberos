dd87ec63b7366c6d5d5f187363e7341e79a93431
Eugene Koontz <ekoontz@hiro-tan.org>
2010-12-29 17:14:14 -0800

    SASLized version of IRC server

--- NIOServerSASL.java
+++ NIOServerSASL.java
@@ -5,7 +5,81 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.NoSuchElementException;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.sasl.AuthorizeCallback;
+import javax.security.sasl.Sasl;
+import javax.security.sasl.SaslException;
+import javax.security.sasl.SaslServer;
+import java.security.PrivilegedExceptionAction;
+import java.security.PrivilegedActionException;
+
+import javax.security.auth.Subject;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+
 public class NIOServerSASL extends NIOServerMultiThread {
+
+  public void authenticateServer() {
+    // Should be called only once, at server startup time.
+    
+    //    System.setProperty("sun.security.krb5.debug", "true");
+    System.setProperty("javax.security.sasl.level","FINEST");
+    System.setProperty("handlers", "java.util.logging.ConsoleHandler");
+    
+    // <Constants>
+    final String JAAS_CONF_FILE_NAME = "jaas.conf";
+    
+    final String HOST_NAME = "ekoontz"; // The hostname that the service (this code) is running on. (might be fully qualified, or not)
+    
+    final String SERVICE_PRINCIPAL_NAME = "testserver"; // The service that we're running with this code.
+                                                        // There must exist a Kerberos principal called
+                                                        // $SERVICE_PRINCIPAL_NAME/$HOSTNAME.
+    
+    final String SERVICE_SECTION_OF_JAAS_CONF_FILE = "Server"; // The section (of the JAAS configuration file named $JAAS_CONF_FILE_NAME)
+
+    final String KEY_TAB_FILE_NAME = "testserver.keytab"; // The file that holds the service's credentials.
+
+    // </Constants>
+
+    System.setProperty( "java.security.auth.login.config", JAAS_CONF_FILE_NAME);
+
+    //
+    // The file given in JAAS_CONF_FILE_NAME must have :
+    //
+    // $SERVICE_SECTION_OF_JAAS_CONF_FILE {
+    //   com.sun.security.auth.module.Krb5LoginModule required
+    //   useKeyTab=true
+    //   keyTab="$KEY_TAB_FILE_NAME"
+    //   doNotPrompt=true
+    //   useTicketCache=false
+    //   storeKey=true
+    //   debug=true
+    //   principal="$SERVICE_NAME/$HOST_NAME";
+    // };
+
+    try {
+      final Subject subject;
+      
+      // 1. Login to Kerberos.
+      LoginContext loginCtx = null;
+      System.out.println("Authenticating using '" + SERVICE_SECTION_OF_JAAS_CONF_FILE + "' section of '" + JAAS_CONF_FILE_NAME + "'...");
+      loginCtx = new LoginContext(SERVICE_SECTION_OF_JAAS_CONF_FILE);
+      loginCtx.login();
+      subject = loginCtx.getSubject();
+
+      System.out.println("..authenticated.");
+    }
+    catch (LoginException e) {
+      System.err.println("LoginException: : " + e);
+      e.printStackTrace();
+      System.exit(-1);
+    }
+
+
+    
+  }
  
   public static void main(String[] args) 
     throws IOException {
@@ -19,6 +93,8 @@ public class NIOServerSASL extends NIOServerMultiThread {
     int localPort = Integer.parseInt(args[0]);
 
     NIOServerSASL instance = new NIOServerSASL();
+
+    instance.authenticateServer();
 
     try {
       instance.StartThreadsAndRun(localPort);
